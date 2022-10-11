26d9a9768927e63c6620cff2add9fb8620ae9280
MaeFire <masked_phoenix@yahoo.com>
2022-10-11 03:33:55 -0500

    Capture player selection
    Buttons linked between HTML and JS file. ID used to declare choice

--- rps-game.js
+++ rps-game.js
@@ -1,8 +1,9 @@
 /* 
-Computer makes a random choice between rock, paper, scissors.
-Player is prompted to make a choice between rock, paper, scissors.
-rock<paper<scissors<rock If rock and paper are chosen, then return paper wins. Repeat until complete.
-If wins equal 5, return 'you win!'
+Rock, Paper, Scissors
+define variables to use/being used
+create basic round of gameplay
+use that round for the game
+report round results, and game results, in console
 */
 
 let computerScore     = 0;
@@ -12,6 +13,7 @@ let choices           = ["rock", "paper", "scissors"];
 let playerSelection   ='';
 let computerSelection ='';
 let gameOutcome       ='';
+let buttons = document.querySelectorAll(".btn")
 
 function playRound(playerSelection, computerSelection) {
     if(
@@ -20,37 +22,55 @@ function playRound(playerSelection, computerSelection) {
         (computerSelection === "scissors" && playerSelection === "paper")
         ){
             computerScore++
-            roundOutcome ='Oh no! The computer wins this round!',console.log(playerScore,computerScore);
+            roundOutcome ="Oh no! The computer wins this round!",console.log(playerScore,computerScore);
             return (roundOutcome);
         }else if (
             (computerSelection === playerSelection)
             ){
-                roundOutcome= 'Round tied!',console.log(playerScore,computerScore);
+                roundOutcome= "Round tied!",console.log(playerScore,computerScore);
                 return (roundOutcome);
             }
         else {
                 playerScore++
-                roundOutcome = 'You win this round!',console.log(playerScore,computerScore);
+                roundOutcome = "You win this round!",console.log(playerScore,computerScore);
                 return (roundOutcome);
         }  
 }
 
-function game(){
+
+
+buttons.forEach(function (item){
+    item.addEventListener('click', function (getPlayerChoice){
+        let playerSelection = (getPlayerChoice.target.id);
+        playRound(playerSelection,computerSelection);
+    });
+});
+
+
+    /* function game(){
+        
+        for(roundTotal = 0; roundTotal < 5; roundTotal++){
+            let computerSelection = choices[Math.floor(Math.random() * choices.length)];
+            let playerSelection = prompt("Choose Rock, Paper, or Scissors");
+            console.log(roundTotal,playRound(playerSelection,computerSelection));
+        }
+        if(playerScore > computerScore){
+            gameOutcome = "Congratulations! You win!";
+            return (gameOutcome);
+        }else if(computerScore > playerScore){
+            gameOutcome = "Congra- Oh. Nevermind. The computer wins!";
+            return (gameOutcome);
+        }else
+            gameOutcome = "The computer showed mercy. Game draw.";
+            return (gameOutcome);
+    } 
+    console.log(game());
+    console.log(computerScore,playerScore);*/
     
-    for(roundTotal = 0; roundTotal < 5; roundTotal++){
-        let computerSelection = choices[Math.floor(Math.random() * choices.length)];
-        let playerSelection = prompt("Choose Rock, Paper, or Scissors");
-        console.log(roundTotal,playRound(playerSelection,computerSelection));
-    }
-    if(playerScore > computerScore){
-        gameOutcome = "Congratulations! You win!";
-        return (gameOutcome);
-    }else if(computerScore > playerScore){
-        gameOutcome = "Congra- Oh. Nevermind. The computer wins!";
-        return (gameOutcome);
-    }else
-        gameOutcome = "The computer showed mercy. Game draw.";
-        return (gameOutcome);
-}
-console.log(game());
-console.log(computerScore,playerScore);
+    
+    /*const removeFromArray = function(...theArgs) {
+        const array = theArgs[0];
+        return array.filter(value => !theArgs.includes(value));
+    };*/
+    
+    //document.getElementById("rock").addEventListener("click",playRound);
