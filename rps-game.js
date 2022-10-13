5c8d3fa877b01b9ac5cac1b7fe5f06dd51162545
MaeFire <masked_phoenix@yahoo.com>
2022-10-13 01:48:37 -0500

    Rewrite working code for efficiency
    Everything is more or less the same in functionality. Game now runs
    on a boolean check, allowing for easier changes to be made.
    
    Update possible with all credit of this update belonging to Xanide.

--- rps-game.js
+++ rps-game.js
@@ -1,67 +1,35 @@
-/* 
-Rock, Paper, Scissors
-define variables to use/being used
-create basic round of gameplay
-use that round for the game
-report round results, and game results, in console
-*/
-
 let computerScore     = 0;
 let playerScore       = 0;
 
-let roundOutcome      ='';
-let gameOutcome       ='';
+let gameRunning      = true;
 
-let playerTurn           ;
-let computerSelection ='';
-
-const choices         = ["rock", "paper", "scissors"];
 const buttons         = document.querySelectorAll("div.buttonChoice button");
 
-function computerTurn(){
-    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
-    return computerSelection;
-};
-
-
-buttons.forEach(button =>{button.addEventListener('click', playerChoice)});
-
-function playRound(playerSelection, computerSelection) {
-    if(
-        (computerSelection === "rock" && playerSelection === "scissors")||
-        (computerSelection === "paper" && playerSelection === "rock")||
-        (computerSelection === "scissors" && playerSelection === "paper")
-        ){
-            computerScore++
-            roundOutcome ="Oh no! The computer wins this round!",console.log(playerScore,computerScore);            
-        }else if ((computerSelection === playerSelection)){
-                roundOutcome= "Round tied!",console.log(playerScore,computerScore);               
-        }else {
-                playerScore++
-                roundOutcome = "You win this round!",console.log(playerScore,computerScore);         
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        if (gameRunning) {
+            let playerSelection = parseInt(e.target.value);
+            let computerSelection = Math.floor(Math.random() * 3);
+            if (
+                    (computerSelection === 1 && playerSelection === 3) ||
+                    (computerSelection === 2 && playerSelection === 1) ||
+                    (computerSelection === 3 && playerSelection === 2)) {
+                computerScore++;
+                console.log("Oh no! The computer wins this round!\n", playerScore, computerScore);            
+                if (computerScore >= 5) {
+                    gameRunning = false;
+                    console.log("You lost? Seriously? This thing should have been programmed to lose!");
+                }
+            } else if ((computerSelection === playerSelection)) {
+                console.log("Round tied!\n", playerScore,computerScore);               
+            } else {
+                playerScore++;
+                console.log("You win this round!\n", playerScore, computerScore);         
+                if (playerScore >= 5) {
+                    gameRunning = false;
+                    console.log("You showed that Skynet wannabe who the real machine is!");
+                }
+            }
         }
-        console.log(roundOutcome);
-        endGameResults();
-        console.log(endGameResults());
-    };
-
-
-function playerChoice(e){
-        let playerSelection = (e.target.id);
-        playerTurn = e.target.id;
-        playRound(playerSelection,computerTurn());        
-    };
-
-function endGameResults(){
-    if (playerScore === 5 && computerScore !== 5){
-        gameOutcome = "You showed that Skynet wannabe who the real machine is!";
-        buttons.forEach(button => {button.removeEventListener('click', playerChoice);
     });
-        return (gameOutcome);
-    }else if (playerScore !== 5 && computerScore === 5){
-        gameOutcome = "You lost? Seriously? This thing should have been programmed to lose!";
-        buttons.forEach(button => {button.removeEventListener('click', playerChoice);
-    });
-        return (gameOutcome);
-    }        
-};
+});
